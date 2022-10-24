89068dd3ce569dafa8e7b67c585064bf66584615
trung <trungntfx16444@gmail.com>
2022-10-25 06:35:30 +0700

    update server

--- App/config/db.config.js
+++ App/config/db.config.js
@@ -7,10 +7,20 @@ module.exports = {
   dialect: "mysql",
 };
 */
+/*
 module.exports = {
   HOST: "remotemysql.com",
   USER: "yHNF44ZW0u",
   PASSWORD: "S0ipMnfyOJ",
   DB: "yHNF44ZW0u",
+  dialect: "mysql",
+};
+*/
+
+module.exports = {
+  HOST: "sql6.freemysqlhosting.net",
+  DB: "sql6528850",
+  USER: "sql6528850",
+  PASSWORD: "WBJxTKyezE",
   dialect: "mysql",
 };
